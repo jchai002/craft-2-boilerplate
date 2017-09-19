045fb1cabe590400f99693e02eeb5118d4b72f81
Jerry Chai <jerry@gooddogdesign.com>
2017-09-19 10:06:02 -0700

    restructure scripts and styles src

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,59 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    bulkSass     = require('gulp-sass-bulk-import'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cleanCss     = require('gulp-clean-css'),
-    uglify       = require('gulp-uglify'),
-    concat       = require('gulp-concat'),
-    rename       = require('gulp-rename'),
-    size         = require('gulp-size');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	bulkSass = require('gulp-sass-bulk-import'),
+	sourcemaps = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cleanCss = require('gulp-clean-css'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	size = require('gulp-size');
 
-var scssSource = 'src/styles/';
+var scssSource = '_src/styles/';
 var cssDest = 'public/assets/css/';
-var jsSource = 'src/scripts/';
+var jsSource = '_src/scripts/';
 var jsDest = 'public/assets/js/';
 
 gulp.task('build-css', function() {
-  console.log('buidling css..')
-  gulp.src(scssSource + 'main.scss')
-  .pipe(bulkSass())
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer({
-    browsers: ['last 25 versions']
-  }))
-  .pipe(cleanCss())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest(cssDest))
+	console.log('buidling css..');
+	gulp
+		.src(scssSource + 'manifest.scss')
+		.pipe(bulkSass())
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(
+			autoprefixer({
+				browsers: ['last 25 versions']
+			})
+		)
+		.pipe(cleanCss())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(cssDest));
 });
 
 gulp.task('build-js', function() {
-  gulp.src([
-    jsSource+'core/jquery-3.0.0.js',
-    jsSource+'core/tether.min.js',
-    jsSource+'core/bootstrap.min.js',
-    jsSource+'vendor/**/*'
-  ])
-  .pipe(concat('main.js'))
-  .pipe(gulp.dest(jsDest))
-  // create minified version for production use
-  .pipe(uglify())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest(jsDest))
-  .pipe(size({gzip: true, showFiles: true}));
+	gulp
+		.src([
+			jsSource + 'core/jquery-3.0.0.js',
+			jsSource + 'core/tether.min.js',
+			jsSource + 'core/bootstrap.min.js',
+			jsSource + 'vendor/**/*',
+			jsSource + 'modules/**/*',
+			jsSource + 'app.js'
+		])
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest(jsDest))
+		// create minified version for production use
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(jsDest))
+		.pipe(size({ gzip: true, showFiles: true }));
 });
 
-gulp.task('watch', function(){
-  gulp.watch(scssSource+ '**/*.scss', ['build-css']);
-  gulp.watch(jsSource +'**/*',['build-js']);
+gulp.task('watch', function() {
+	gulp.watch(scssSource + '**/*.scss', ['build-css']);
+	gulp.watch(jsSource + '**/*', ['build-js']);
 });
 
 gulp.task('default', ['build-css', 'build-js', 'watch']);
